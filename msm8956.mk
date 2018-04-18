cee05d1b211ace0e38f3ee140ee03570a9b0950a
LokManSiu <bjsiu0617@gmail.com>
2018-04-18 12:47:35 +0800

    Update for Snap camera app define

--- msm8956.mk
+++ msm8956.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
-    SnapdragonCamera
+    Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
